bb9846295e97382256d6db2732193401dbc4f1ba
vagrant <km.dwc23@gmail.com>
2020-11-15 06:19:28 +0000

    [Finish] 3-50

---
+++ 030_スコープ/050_クロージャー（プライベート変数）/start/main.js
@@ -0,0 +1,20 @@
+
+
+function incrementFactory() {
+  
+  let num = 0;
+
+  function increment() {
+    num = num + 1;
+    console.log(num);
+  }
+
+  return increment;
+}
+
+const increment = incrementFactory();
+
+increment();
+increment();
+increment();
+increment();
