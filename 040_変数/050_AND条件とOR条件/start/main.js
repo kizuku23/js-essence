9ed4a25934fdfb067d0f2a9b4f06ff3856a241bb
vagrant <km.dwc23@gmail.com>
2020-11-16 08:39:24 +0000

    [Finish] 4-51

--- 040_変数/050_AND条件とOR条件/start/main.js
+++ 040_変数/050_AND条件とOR条件/start/main.js
@@ -4,6 +4,6 @@ const c = 3;
 const d = 0;
 console.log(a && b && c); // falsyが見つかったら結果として返す　見つからない場合は最後の値
 console.log(a || b || c); // truesyが見つかったら結果として返す
-console.log((a || b) && c);
+console.log((a || b) && c); // 見てわかるようにグループ化
 console.log((a || b) && (c || d));
 //||
