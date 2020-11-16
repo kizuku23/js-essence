f347ebb228484d8eac8d44eace9eca7118bd1a66
vagrant <km.dwc23@gmail.com>
2020-11-16 08:06:37 +0000

    [Finish] 4-30

---
+++ 040_変数/030_厳格な等価性と抽象的な等価性/start/main.js
@@ -0,0 +1,22 @@
+function pringEquality(a, b) {
+  console.log(a === b);
+  console.log(a == b);
+}
+
+let a = '1';
+let b = 1;
+
+let c = true;
+
+//pringEquality(b, c);
+
+let e = "";
+let f = 0;
+
+//pringEquality(e,f);
+
+let g = null;
+let h;
+
+pringEquality(g, h);
+
