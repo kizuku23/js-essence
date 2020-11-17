e57ab02d93b1f0a213b4bb80e7ec7b2623e459ed
vagrant <km.dwc23@gmail.com>
2020-11-17 08:16:30 +0000

    [Finish] 5-10

---
+++ 050_関数とオブジェクト１/010_関数/start/main.js
@@ -0,0 +1,11 @@
+function fn(...args) {
+  console.log(args);
+  const a = arguments[0];
+  const b = arguments[1];
+  console.log(arguments);
+  console.log(a,b);
+  return a;
+}
+
+let c = fn(1, undefined, 0);
+console.log(c);
