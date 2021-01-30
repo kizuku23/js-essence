9775987c3c700b7879b364d194fc6063c7054b38
vagrant <km.dwc23@gmail.com>
2021-01-30 14:35:56 +0000

    [Finish] 4-6-2

---
+++ 040_変数/060_AND条件とOR条件（応用）/start2/main.js
@@ -0,0 +1,16 @@
+function hello(name) {
+  name = name || 'Tom';
+
+  // if(!name) {
+  //   name = 'Tom'
+  // }
+  console.log('hello' + name);
+}
+
+hello();
+
+let name// = 'Bob';
+
+// name && hello(name);
+// console.log(name);
+
